6b3a58c3e1e7ffcd84ddb6330531d32d080e124d
pauloborges <contrictorhkss@gamil.com>
2018-08-29 22:51:58 -0300

    feat(service): pessoa, documento e estado federativo

--- backend/src/main/java/br/med/maisvidas/service/DocumentoService.java
+++ backend/src/main/java/br/med/maisvidas/service/DocumentoService.java
@@ -1,6 +1,39 @@
 package br.med.maisvidas.service;
 
+import br.med.maisvidas.model.Documento;
+import br.med.maisvidas.model.Pessoa;
+import br.med.maisvidas.repository.DocumentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class DocumentoService {}
+public class DocumentoService {
+
+    @Autowired
+    private DocumentoRepository documentoRepository;
+
+    public List<Documento> listarDocumentosPessoa(int idPessoa) {
+        Pessoa pessoa =  new Pessoa();
+        pessoa.setIdPessoa(idPessoa);
+        return this.documentoRepository.findAllByPessoa(pessoa);
+    }
+
+    public void remover(int idDocumento) {
+        this.documentoRepository.deleteByIdDocumento(idDocumento);
+    }
+
+    public Documento atualizar(int idDocumento, Documento documento) {
+        documento.setIdDocumento(idDocumento);
+        return this.documentoRepository.save(documento);
+    }
+
+    public Documento criar(Documento documento) {
+        return this.documentoRepository.save(documento);
+    }
+
+    public Documento listarDocumento(int idDocumento) {
+        return this.documentoRepository.findByIdDocumento(idDocumento);
+    }
+}
--- backend/src/main/java/br/med/maisvidas/service/PessoaService.java
+++ backend/src/main/java/br/med/maisvidas/service/PessoaService.java
@@ -1,8 +1,6 @@
 package br.med.maisvidas.service;
 
-import br.med.maisvidas.model.Estado;
 import br.med.maisvidas.model.Pessoa;
-import br.med.maisvidas.repository.EstadoRepository;
 import br.med.maisvidas.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,5 +15,22 @@ public class PessoaService {
 
     public List<Pessoa> listarTodasPessoas() {
         return this.pessoaRepository.findAll();
+    }
+
+    public Pessoa listarPessoa(int idPessoa) {
+        return this.pessoaRepository.findByIdPessoa(idPessoa);
+    }
+
+    public void remover(int idPessoa) {
+        this.pessoaRepository.deleteByIdPessoa(idPessoa);
+    }
+
+    public Pessoa atualizar(int idPessoa, Pessoa Pessoa) {
+        Pessoa.setIdPessoa(idPessoa);
+        return this.pessoaRepository.save(Pessoa);
+    }
+
+    public Pessoa criar(Pessoa Pessoa) {
+        return this.pessoaRepository.save(Pessoa);
     }
 }
